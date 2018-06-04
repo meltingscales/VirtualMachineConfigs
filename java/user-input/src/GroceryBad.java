e7d0d6ea5db628cae64345150c91ec04e8b05dad
HenryFBP <HenryFBP@gmail.com>
2018-06-04 17:12:31 -0400

    finalize 'bad' grocery example

--- java/user-input/src/GroceryBad.java
+++ java/user-input/src/GroceryBad.java
@@ -4,12 +4,11 @@ public class GroceryBad
 {
     public static void main(String[] args)
     {
-        // This is what I mean:
+        // This is what I meant in the other file:
 
         // If you want a new item, like bananas, you must add three new variables.
 
-        // Not only is that inconvenient, but there is no way to easily access
-        // these values!
+        // Not only is that inconvenient, but there is no way to easily access these values!
         int appleAmt = 0;
         int cherryAmt = 0;
 
@@ -19,24 +18,35 @@ public class GroceryBad
         String appleUnit = "pound";
         String cherryUnit = "pound";
 
+        System.out.println("Enter -1 to quit.");
+        int i = 0;
+        System.out.println(i++ + " for apples," + i++ + " for cherries.");
+
         int input = 1;
         Scanner s = new Scanner(System.in);
 
-        while(input > 0)
+        while(input >= 0)
         {
+            System.out.print(" > ");
+            input = s.nextInt();
+
             switch(input)
             {
-            case 1: //These numbers are hard-coded, too.
+            case 0: // These numbers are hard-coded, too.
             {
                 appleAmt++;
+                break;
             }
-            case 2:
+            case 1:
             {
                 cherryAmt++;
+                break;
             }
             }
         }
-
-        System.out.printf("%d %ss at %.2f",appleAmt,);
+        // As are these print statements!
+        // Super hard to add stuff, huh?
+        System.out.printf("%d %ss at %.2f per %s\n", appleAmt, "apples", appleAmt * applePrice, appleUnit);
+        System.out.printf("%d %ss at %.2f per %s\n", cherryAmt, "cherries", cherryAmt * cherryPrice, cherryUnit);
     }
 }
