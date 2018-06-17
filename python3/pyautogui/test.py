dfc5ca6c44e253e4b2ae859c1fbb77f03fbd81d5
HenryFBP <HenryFBP@gmail.com>
2018-06-17 15:33:05 -0400

    change into a class-based system

--- python3/pyautogui/test.py
+++ python3/pyautogui/test.py
@@ -13,38 +13,42 @@ def copy(w: WindowSpecification, k='^c'):
     return win32clipboard.GetClipboardData()
 
 
-def binary_op(calc: Application, x, y, op='{+}'):
-    calc.CalcFrame.TypeKeys(str(x) + op + str(y) + '{ENTER}')
-    return copy(calc.CalcFrame)
+class Calculator(object):
+    window: Application
+
+    def __init__(self):
+        try:
+            self.window = Application().connect(title='Calculator')
+        except ElementNotFoundError as e:
+            self.window = Application().start(cmd_line='calc.exe')
+
+    def binary_op(self, x, y, op='{+}'):
+        self.window.CalcFrame.TypeKeys(str(x) + op + str(y) + '{ENTER}')
+        return copy(self.window.CalcFrame)
+
+    def add(self, x, y):
+        return self.binary_op(x, y, '{+}')
+
+    def sub(self, x, y):
+        return self.binary_op(x, y, '{-}')
+
+    def mult(self, x, y):
+        return self.binary_op(x, y, '{*}')
+
+    def div(self, x, y):
+        return self.binary_op(x, y, '{/}')
+
+    def clear(self):
+        for i in range(0, 2):
+            self.window.CalcFrame.TypeKeys('{ESC}')
 
 
-def add(calc: Application, x, y):
-    return binary_op(calc, x, y, '{+}')
+calc = Calculator()
 
-
-def sub(calc: Application, x, y):
-    return binary_op(calc, x, y, '{-}')
-
-
-def mult(calc: Application, x, y):
-    return binary_op(calc, x, y, '{*}')
-
-
-def clear(calc: Application):
-    calc.CalcFrame: WindowSpecification
-    for i in range(0, 3):
-        calc.CalcFrame.TypeKeys('{ESC}')
-
-
-try:
-    calc = Application().connect(title='Calculator')
-except ElementNotFoundError as e:
-    calc = Application().start(cmd_line='calc.exe')
-
-clear(calc)
-x = add(calc, 1, 2)
+calc.clear()
+x = calc.add(1, 2)
 print(x)
 
 for i in range(0, 10):
-    x = mult(calc, i, i + 1)
+    x = calc.mult(i, i + 1)
     print(x)
