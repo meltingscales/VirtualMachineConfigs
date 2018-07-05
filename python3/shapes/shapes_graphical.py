d33dcc4f9bdc8f7b1e62595df89fe5574fa008dc
HenryFBP <HenryFBP@gmail.com>
2018-07-05 19:03:51 -0400

    reformat code

--- python3/shapes/shapes_graphical.py
+++ python3/shapes/shapes_graphical.py
@@ -4,21 +4,24 @@ from tkinter import *
 class MandelImage(PhotoImage):
     pass
 
+
 def up_key(event):
-  print("WHO PRESSED UP?")
+    print("WHO PRESSED UP?")
+
 
 def mandel(kx, ky, color):
-  """ calculates the pixel color of the point of mandelbrot plane
-      passed in the arguments """
+    """ calculates the pixel color of the point of mandelbrot plane
+        passed in the arguments """
 
-  maxIt = 256
-  c = complex(kx, ky)
-  z = complex(0.0, 0.0)
-  for i in range(maxIt):
-      z = z * z + c
-      if abs(z) >= 2.0:
-         return (255-color[i],0,0)
-  return(0,0,0)
+    maxIt = 256
+    c = complex(kx, ky)
+    z = complex(0.0, 0.0)
+    for i in range(maxIt):
+        z = z * z + c
+        if abs(z) >= 2.0:
+            return (255 - color[i], 0, 0)
+    return (0, 0, 0)
+
 
 def prepare_mdb(x, y, dim):
     """ pre-calculates coordinates of the mandelbrot plane required for each
@@ -32,19 +35,20 @@ def prepare_mdb(x, y, dim):
 
     return [xm, ym]
 
-def display_mandelbrot(canvas, color, dim=[640, 480], x=[-2.0, 1.0], y=[-1.5, 1.5]):
 
+def display_mandelbrot(canvas, color, dim=[640, 480], x=[-2.0, 1.0], y=[-1.5, 1.5]):
     w, h = dim
 
-    xm, ym = prepare_mdb(x, y, dim) # Make all possible x,y coords.
+    xm, ym = prepare_mdb(x, y, dim)  # Make all possible x,y coords.
 
-    image = PhotoImage(width = w, height = h)
+    image = PhotoImage(width=w, height=h)
 
-    canvas.create_image((0, 0), image = image, state = "normal", anchor = NW)
+    canvas.create_image((0, 0), image=image, state="normal", anchor=NW)
 
-    pixels=" ".join(("{"+" ".join(('#%02x%02x%02x' % mandel(i, j, color) for i in xm))+"}" for j in ym))
+    pixels = " ".join(("{" + " ".join(('#%02x%02x%02x' % mandel(i, j, color) for i in xm)) + "}" for j in ym))
 
     image.put(pixels)
+
 
 if __name__ == '__main__':
     root = Tk()
@@ -52,22 +56,22 @@ if __name__ == '__main__':
     dim = [640, 480]
     w, h = dim
 
-    #corners of  the mandelbrot plan to display  
+    # corners of  the mandelbrot plan to display
     x = [-2.0, 1.0]
     y = [-1.5, 1.5]
 
-    #precalculated color table
-    color=[ int(255*(i/255)**12) for i in range(255,-1,-1)]
+    # precalculated color table
+    color = [int(255 * (i / 255) ** 12) for i in range(255, -1, -1)]
     xm, ym = prepare_mdb(x, y, dim)
 
-    #first method, w/o function...
+    # first method, w/o function...
     window = Toplevel(master=root)
     canvas = Canvas(window, width=w, height=h, bg="#000000")
 
-    img = PhotoImage(width = w, height = h)
-    canvas.create_image((0, 0), image = img, state = "normal", anchor = NW)
+    img = PhotoImage(width=w, height=h)
+    canvas.create_image((0, 0), image=img, state="normal", anchor=NW)
 
-    pixels=" ".join(("{"+" ".join(('#%02x%02x%02x' % mandel(i, j, color) for i in xm))+"}" for j in ym))
+    pixels = " ".join(("{" + " ".join(('#%02x%02x%02x' % mandel(i, j, color) for i in xm)) + "}" for j in ym))
 
     img.put(pixels)
 
@@ -78,11 +82,11 @@ if __name__ == '__main__':
     canvas2 = Canvas(window2, width=w, height=h, bg='#000000')
 
     display_mandelbrot(canvas2, color, dim, x, y)
-    
+
     canvas2.pack()
 
     # Callbacks
     window.bind('<Up>', up_key)
 
-    #Mainloop
+    # Mainloop
     mainloop()
