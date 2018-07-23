42e29df3da65be0f944ac0572e9974a6a7b92725
HenryFBP <HenryFBP@gmail.com>
2018-07-22 20:24:10 -0400

    fully migrate game.js to game.ts

--- web/game/js/game.js
+++ web/game/js/game.js
@@ -1,88 +1,93 @@
 var fs = require('fs');
 var path = require('path');
 var yaml = require('js-yaml');
-var controls = yaml.safeLoad(fs.readFileSync('data/controls.yml', 'utf8'));
-var game = new Phaser.Game(500, 500, Phaser.AUTO, 'phaser-example', {
-    preload: preload,
-    create: create,
-    update: update,
-    render: render,
-});
-var player;
 var block;
-var facing = 'left';
-var BUTTON_JUMP;
-var BUTTON_LEFT;
-var BUTTON_RIGHT;
-var BUTTON_UP;
-var BUTTON_DOWN;
-function preload() {
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-    game.load.image('stone', 'assets/stone.png');
-}
-function create() {
-    game.world.setBounds(0, 0, 500, 500);
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.time.desiredFps = 60;
-    game.physics.arcade.gravity.y = 250;
-    player = game.add.sprite(game.world.bounds.width - 100, game.world.bounds.height - 200, 'dude');
-    game.physics.enable(player, Phaser.Physics.ARCADE);
-    game.camera.follow(player);
-    block = game.add.sprite(game.world.bounds.width - 100, game.world.bounds.height - 75, 'stone');
-    game.physics.enable(block, Phaser.Physics.ARCADE);
-    block.body.allowGravity = false;
-    block.body.immovable = true;
-    block.body.allowDrag = false;
-    player.body.bounce.y = 0.2;
-    player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 16);
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-    BUTTON_JUMP = game.input.keyboard.addKey(Phaser.Keyboard[controls['JUMP']]);
-    BUTTON_LEFT = game.input.keyboard.addKey(Phaser.Keyboard[controls['LEFT']]);
-    BUTTON_RIGHT = game.input.keyboard.addKey(Phaser.Keyboard[controls['RIGHT']]);
-    BUTTON_UP = game.input.keyboard.addKey(Phaser.Keyboard[controls['UP']]);
-    BUTTON_DOWN = game.input.keyboard.addKey(Phaser.Keyboard[controls['DOWN']]);
-}
-function collisionHandler(obj1, obj2) {
-    game.debug.text("collision!", 32, 48);
-}
-function update() {
-    // game.physics.arcade.collide(player, layer);
-    game.physics.arcade.collide(player, block, collisionHandler, null, this);
-    player.body.velocity.x = 0;
-    if (BUTTON_LEFT.isDown) {
-        player.body.velocity.x = -150;
-        if (facing != 'left') {
-            player.animations.play('left');
-            facing = 'left';
-        }
+var SimpleGame = /** @class */ (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(500, 500, Phaser.AUTO, 'phaser-example', {
+            preload: this.preload,
+            create: this.create,
+            update: this.update,
+            render: this.render,
+        });
     }
-    else if (BUTTON_RIGHT.isDown) {
-        player.body.velocity.x = 150;
-        if (facing != 'right') {
-            player.animations.play('right');
-            facing = 'right';
-        }
-    }
-    else {
-        if (facing != 'idle') {
-            player.animations.stop();
-            if (facing == 'left') {
-                player.frame = 0;
+    SimpleGame.prototype.preload = function () {
+        this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        this.game.load.image('stone', 'assets/stone.png');
+        this.controls = yaml.safeLoad(fs.readFileSync('data/controls.yml', 'utf8'));
+        this.buttons = {};
+        console.log(this.controls);
+        this.buttons['JUMP'] = this.game.input.keyboard.addKey(Phaser.KeyCode[this.controls['JUMP']]);
+        this.buttons['LEFT'] = this.game.input.keyboard.addKey(Phaser.KeyCode[this.controls['LEFT']]);
+        this.buttons['RIGHT'] = this.game.input.keyboard.addKey(Phaser.KeyCode[this.controls['RIGHT']]);
+        this.buttons['UP'] = this.game.input.keyboard.addKey(Phaser.KeyCode[this.controls['UP']]);
+        this.buttons['DOWN'] = this.game.input.keyboard.addKey(Phaser.KeyCode[this.controls['DOWN']]);
+    };
+    SimpleGame.prototype.create = function () {
+        this.game.world.setBounds(0, 0, 500, 500);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.time.desiredFps = 60;
+        this.game.physics.arcade.gravity.y = 250;
+        this.player = this.game.add.sprite(this.game.world.bounds.width - 100, this.game.world.bounds.height - 200, 'dude');
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.game.camera.follow(this.player);
+        block = this.game.add.sprite(this.game.world.bounds.width - 100, this.game.world.bounds.height - 75, 'stone');
+        this.game.physics.enable(block, Phaser.Physics.ARCADE);
+        block.body.allowGravity = false;
+        block.body.immovable = true;
+        block.body.allowDrag = false;
+        this.player.body.bounce.y = 0.2;
+        this.player.body.collideWorldBounds = true;
+        this.player.body.setSize(20, 32, 5, 16);
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('turn', [4], 20, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+    };
+    SimpleGame.prototype.collisionHandler = function (obj1, obj2) {
+        console.log(obj1);
+        console.log(obj2);
+    };
+    SimpleGame.prototype.render = function () {
+        this.game.debug.text(this.game.time.suggestedFps.toString(), 32, 32);
+    };
+    SimpleGame.prototype.update = function () {
+        // game.physics.arcade.collide(player, layer);
+        var _this = this;
+        this.game.physics.arcade.collide(this.player, block, function (o1, o2) {
+            _this.game.debug.text("collision!", 32, 48);
+        }, null, this);
+        this.player.body.velocity.x = 0;
+        if (this.buttons['LEFT'].isDown) {
+            this.player.body.velocity.x = -150;
+            if (this.facing != 'left') {
+                this.player.animations.play('left');
+                this.facing = 'left';
             }
-            else {
-                player.frame = 5;
-            }
-            facing = 'idle';
         }
-    }
-    if (BUTTON_JUMP.isDown &&
-        (player.body.onFloor() || player.body.touching.down)) {
-        player.body.velocity.y = -250;
-    }
-}
-function render() {
-    game.debug.text(game.time.suggestedFps.toString(), 32, 32);
-}
+        else if (this.buttons['RIGHT'].isDown) {
+            this.player.body.velocity.x = 150;
+            if (this.facing != 'right') {
+                this.player.animations.play('right');
+                this.facing = 'right';
+            }
+        }
+        else {
+            if (this.facing != 'idle') {
+                this.player.animations.stop();
+                if (this.facing == 'left') {
+                    this.player.frame = 0;
+                }
+                else {
+                    this.player.frame = 5;
+                }
+                this.facing = 'idle';
+            }
+        }
+        if (this.buttons['JUMP'].isDown &&
+            (this.player.body.onFloor() || this.player.body.touching.down)) {
+            this.player.body.velocity.y = -250;
+        }
+    };
+    return SimpleGame;
+}());
+var game = new SimpleGame();
