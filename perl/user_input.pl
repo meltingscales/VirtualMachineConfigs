da868c40cbeed8d6ec2e5da2d39d1aa64534404c
HenryFBP <HenryFBP@gmail.com>
2018-10-21 18:06:36 -0500

    make user enter numbers

--- perl/user_input.pl
+++ perl/user_input.pl
@@ -1,14 +1,21 @@
 #!/bin/perl
 use warnings;
 use strict;
+use Scalar::Util qw(looks_like_number);
 
+my $x = "";
+my $y = "";
 
-print "Enter first number:\n > ";
-my $x = <STDIN>;
-chomp $x;
+until (looks_like_number($x)) {
+    print "Enter first number:\n > ";
+    $x = <STDIN>;
+    chomp $x;
+}
 
-print "Enter second number:\n > ";
-my $y = <STDIN>;
-chomp $y;
+until (looks_like_number($y)) {
+    print "Enter second number:\n > ";
+    $y = <STDIN>;
+    chomp $y;
+}
 
 printf "%d + %d = %d", $x, $y, ($x + $y)
