feecbe810ebe1e73415355e476d1131fed4959a6
HenryFBP <HenryFBP@gmail.com>
2018-11-17 15:37:00 -0600

    add switch-case example

---
+++ powershell/switch_case.ps1
@@ -0,0 +1,31 @@
+﻿
+
+$in = "-1";
+
+while(!($in -eq "0")) { # While it is not zero,
+
+    Write-Host -NoNewLine "Enter a choice, 1 or 2. 0 Exits.
+> "
+
+    $in = $Host.UI.ReadLine() # Read line without adding an ugly colon.
+
+    Switch($in)
+    {
+        "0" {break;}
+
+        "1" {
+            Write-Host -NoNewLine "One!`n";
+            break;
+        }
+    
+        "2" {
+            Write-Host -NoNewLine "Two!`n";
+            break;
+        }
+
+        default {
+            "Invalid choice.`n"
+            break;
+        }
+    }
+}
