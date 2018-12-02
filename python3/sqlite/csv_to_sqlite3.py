f93e994f3763537907d178c73c3696782cbfb54a
Henry Post <HenryFBP@gmail.com>
2018-12-02 13:22:33 -0600

    create table

--- python3/sqlite/csv_to_sqlite3.py
+++ python3/sqlite/csv_to_sqlite3.py
@@ -1,3 +1,11 @@
 import sqlite3
 
 connection = sqlite3.connect('my_cool_db.db')
+
+connection.execute('''CREATE TABLE IF NOT EXISTS color_table (
+    id      INTEGER PRIMARY KEY NOT NULL,
+    color   TEXT,
+    number  INTEGER,
+    fpnum   FLOAT
+);
+''')
