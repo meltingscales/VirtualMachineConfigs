e376a9be068a3577d00594412cc16dfc8baff8e5
Henry Post <HenryFBP@gmail.com>
2019-01-06 19:45:47 -0600

    add more more shapes

--- python3/turtles/square.py
+++ python3/turtles/square.py
@@ -21,3 +21,7 @@ left(90)
 
 forward(200)
 left(90)
+
+# Something to note here is how 360 divided by 4 gives you 90...
+# 
+# We can use this to our advantage to generalize our code to produce ANY shape!
--- python3/turtles/user_input.py
+++ python3/turtles/user_input.py
@@ -8,9 +8,6 @@ color('black')
 # Start filling.
 begin_fill()
 
-# Go forward 200 steps, and turn left.
-# If we do this four times, we draw the four lines of a square!
-
 size = input("How big should our circle be? (200 is medium)")
 
 for i in range(0, 4): # Since we use a loop, we can save code and perhaps generalize some aspects of the code.
