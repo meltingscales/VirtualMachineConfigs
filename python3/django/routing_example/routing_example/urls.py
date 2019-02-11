aa4c929e5fdaf31ce9579ba1a9a23c86400f7d25
Henry Post <HenryFBP@gmail.com>
2019-02-11 12:02:20 -0600

    finish small routing example

--- python3/django/routing_example/routing_example/urls.py
+++ python3/django/routing_example/routing_example/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('math', views.math, name='math'),
+    path('templating', views.templating, name='templating'),
 ]
--- python3/django/routing_example/routing_example_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RoutingExampleAppConfig(AppConfig):
-    name = 'routing_example_app'
