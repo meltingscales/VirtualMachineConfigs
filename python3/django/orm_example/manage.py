4a640ed2da62316315a996422073e4ace7ec9479
Henry Post <HenryFBP@gmail.com>
2019-02-18 11:48:50 -0600

    add crispy forms

--- python3/django/orm_example/manage.py
+++ python3/django/orm_example/manage.py
@@ -3,6 +3,21 @@ import os
 import sys
 
 if __name__ == '__main__':
+
+    # Check the user has all of the
+    errors = 0
+
+    try:
+        import crispy_forms
+    except ImportError:
+        print("Couldn't import `crispy_forms`.")
+        print("Run `pip install django-cripsy-forms` to install it.")
+        errors += 1
+
+    if errors:
+        print("Exited with {} errors.".format(errors))
+        exit(1)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm_example.settings')
     try:
         from django.core.management import execute_from_command_line
--- python3/django/orm_example/orm_example/settings.py
+++ python3/django/orm_example/orm_example/settings.py
@@ -17,7 +17,6 @@ from os import path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orm_example'
+    'crispy_forms',
+    'orm_example',
 ]
 
 MIDDLEWARE = [
@@ -74,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'orm_example.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -84,7 +82,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -104,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -117,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
