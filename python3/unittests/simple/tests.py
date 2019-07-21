b2e9bff990ae4d896c92a59b1df623e7ef313956
HenryFBP <HenryFBP@gmail.com>
2019-07-21 12:40:46 -0500

    remove unnecessary main

--- python3/unittests/simple/tests.py
+++ python3/unittests/simple/tests.py
@@ -20,14 +20,5 @@ class SubtractNumbersTest(TestCase):
     def testSimple(self):
         self.assertEqual(sub_numbers(3, 2), 1)
 
-
-if __name__ == '__main__':
-    test_suite = unittest.TestSuite()
-
-    # Add test suites
-    test_suite.addTest(AddNumbersTest)
-    test_suite.addTest(SubtractNumbersTest)
-
-    runner = unittest.TextTestResult()
-
-    runner.run(test_suite)
+if __name__ == "__main__":
+    print("Run 'python -m unittest' to run these tests!")
