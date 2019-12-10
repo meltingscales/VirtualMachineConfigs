82f6d83875f1cb573d4f60cda271c996f23d6970
Henry Post <HenryFBP@gmail.com>
2019-12-09 21:52:54 -0600

    -y, always run

--- vagrant/windows-steam/scripts/install-stuff.ps1
+++ vagrant/windows-steam/scripts/install-stuff.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install steam chromium
+choco install -y steam chromium
