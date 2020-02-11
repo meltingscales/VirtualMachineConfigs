0ffb06a2d06f9fe6d690da69aceaf8161fa6b4f6
Henry Post <henryfbp@gmail.com>
2020-02-10 19:00:28 -0600

    add useful insecure web apps

--- kali/scripts/clone-cool-git-repos.sh
+++ kali/scripts/clone-cool-git-repos.sh
@@ -10,6 +10,9 @@ pushd Github
 
 git clone https://github.com/philipperemy/tensorflow-1.4-billion-password-analysis
 
+git clone https://github.com/skyblueee/sqli-labs-php7
+git clone https://github.com/WebGoat/WebGoat
+
 popd
 
 chown -R vagrant:vagrant /home/vagrant/Github
