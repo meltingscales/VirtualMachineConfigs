3bdfa56d96cc983c6bbd94a63ef776d9cb5d7c62
Henry Post <henryfbp@gmail.com>
2020-03-17 10:51:50 -0500

    clone more insecure web app repos

--- vagrant/kali/scripts/clone-cool-git-repos.sh
+++ vagrant/kali/scripts/clone-cool-git-repos.sh
@@ -12,6 +12,23 @@ git clone https://github.com/philipperemy/tensorflow-1.4-billion-password-analys
 
 git clone https://github.com/skyblueee/sqli-labs-php7
 git clone https://github.com/WebGoat/WebGoat
+git clone https://github.com/bkimminich/juice-shop
+git clone https://github.com/jehy-security/bwapp
+git clone https://github.com/hclproducts/AltoroJ
+
+mkdir -p bwapp-docker
+pushd bwapp-docker
+
+rm start-bwapp-docker.sh
+cat > ./start-bwapp-docker.sh <<EOF
+#!/usr/bin/env bash
+
+docker run -d -p 80:80 raesene/bwapp;
+echo "go to http://localhost:80/install.php to see BWAPP :)"'
+
+EOF
+
+popd #bwapp-docker
 
 popd
 
