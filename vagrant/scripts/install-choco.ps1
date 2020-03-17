85ec1646f3f16e33ac06a9d9ee758718f42641ad
Henry Post <HenryFBP@gmail.com>
2020-03-16 19:01:38 -0500

    need gs

--- vagrant/scripts/install-choco.ps1
+++ vagrant/scripts/install-choco.ps1
@@ -1,7 +1,10 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco feature enable -n allowGlobalConfirmation
+
 choco install -y GoogleChrome
 choco install -y notepadplusplus
 choco install -y git
 choco install -y atom
-choco install -y sublime
+choco install -y ghostscript
+choco install -y imagemagick
