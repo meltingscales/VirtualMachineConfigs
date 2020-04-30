8633d0ac0f2d441c701bb1b1184abaf81627990b
Henry Post <henryfbp@gmail.com>
2020-04-30 17:57:29 -0500

    add script just for choco

---
+++ vagrant/scripts/install-choco.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco feature enable -n allowGlobalConfirmation
