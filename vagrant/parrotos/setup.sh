df70b2fc1d016a934e6b8819da3aad794f5c4fef
Henry Post <HenryFBP@gmail.com>
2021-12-30 10:49:54 -0500

    work on setup script

--- vagrant/parrotos/setup.sh
+++ vagrant/parrotos/setup.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+SSH_KEY_LOCATION=/home/vagrant/.ssh/id_rsa
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root. Try 'sudo $0'."
    exit 1
 fi
 
+if [[ ! -f $SSH_KEY_LOCATION ]]; then
+    echo "Please generate or import an SSH key at '$SSH_KEY_LOCATION' with 'ssh-keygen'. 
+This is required for cloning Git repositories.
+After doing so, make sure to import it into GitHub."
+    exit 1
+fi
+
+
 apt-get update
+
+if [[ ! -d ~/Git/ ]]; then
+    mkdir ~/Git/
+fi
+
+
+pushd ~/Git/
+    git clone git@github.com:HenryFBP/hackthebox.git
+    git clone git@github.com:HenryFBP/examples.git
+popd
 
 printf "lol hello\n"
