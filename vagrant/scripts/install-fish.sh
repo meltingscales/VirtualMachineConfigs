b7be546090ce049d1e8ba059b430a9ab6771f97c
Henry Post <HenryFBP@gmail.com>
2022-03-04 22:11:22 +0000

    have vagrant run omf, not root

--- vagrant/scripts/install-fish.sh
+++ vagrant/scripts/install-fish.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+NONROOT_USER=vagrant
+
 apt install -y fish
 echo 'vagrant' | chsh vagrant -s /usr/bin/fish
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > omf-install.fish
 chmod +x omf-install.fish
-./omf-install.fish --noninteractive --yes || echo 'omf install either failed or omf is already installed.'
+sudo -u $NONROOT_USER fish -c "./omf-install.fish --noninteractive --yes || echo 'omf install either failed or omf is already installed.'"
+
 rm ./omf-install.fish
 
 # can replace this with whatever theme you want...
-fish -c "omf install batman"
-fish -c "omf theme batman"
+sudo -u $NONROOT_USER fish -c "omf install batman"
+sudo -u $NONROOT_USER fish -c "omf theme batman"
