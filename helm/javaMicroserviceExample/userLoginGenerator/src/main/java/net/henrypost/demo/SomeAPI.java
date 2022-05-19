635b3e863d884de9c2264ef4ed744bf0bc27e65f
henryfbp <HenryFBP@gmail.com>
2022-05-19 18:24:55 -0400

    get silly ouroboros api to work

---
+++ helm/javaMicroserviceExample/userLoginGenerator/src/main/java/net/henrypost/demo/SomeAPI.java
@@ -0,0 +1,38 @@
+package net.henrypost.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class SomeAPI {
+
+    @GetMapping(path = "/", produces = "application/json")
+    public ResponseEntity<Map<?,?>> index() {
+        Map<String,String> data = new HashMap<String, String>();
+
+        data.put("msg", "welcome to da index brudda");
+        data.put("path", "/");
+
+        return ResponseEntity
+                .ok()
+                .body(data);
+    }
+
+
+    @GetMapping(path = "/someresource", produces = "application/json")
+    public ResponseEntity<Map<?,?>> getSomethingidk() {
+        Map<String,String> data = new HashMap<String, String>();
+
+        data.put("someparamidk", "1234");
+
+        return ResponseEntity
+                .ok()
+                .body(data);
+    }
+}
--- helm/javaMicroserviceExample/userLoginGenerator/src/main/java/net/henrypost/demo/SomeMainClass.java
+++ helm/javaMicroserviceExample/userLoginGenerator/src/main/java/net/henrypost/demo/SomeMainClass.java
@@ -10,31 +10,26 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableScheduling
-public class DemoApplication {
+public class SomeMainClass {
 
-	@Bean
-	public WebClient.Builder getWebClientBuilder() {
-		return WebClient.builder();
-	}
-
-	@Autowired
-	private WebClient.Builder webClientBuilder;
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	// every 5 seconds
 	@Scheduled(cron = "*/5 * * * * *")
 	public void cronJobSch() throws Exception {
+
 		System.out.println("wow its a phucking cron job :3c");
 
-		 product = webClientBuilder.build()
-            .get()
-            .uri("http://localhost:8080/api/products")
-            .retrieve();
-			
+		String response = WebClient.builder().build()
+				.get()
+				.uri("http://localhost:8080/api/someresource")
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
 
+		System.out.println(response);
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeMainClass.class, args);
 	}
 
 }
