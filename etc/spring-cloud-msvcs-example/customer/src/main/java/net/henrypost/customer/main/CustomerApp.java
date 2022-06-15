3fa5de35396bca4661e3a0c4fdf1749a32cd70d3
Henry Post <HenryFBP@gmail.com>
2022-06-15 01:16:06 +0000

    reformat code

--- etc/spring-cloud-msvcs-example/customer/src/main/java/net/henrypost/customer/main/CustomerApp.java
+++ etc/spring-cloud-msvcs-example/customer/src/main/java/net/henrypost/customer/main/CustomerApp.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerApp {
-
-
     public static void main(String[] args) {
-        SpringApplication.run(CustomerApp.class,args);
+        SpringApplication.run(CustomerApp.class, args);
     }
 
 }
--- etc/spring-cloud-msvcs-example/customer/src/main/java/net/henrypost/customer/repository/CustomerRepository.java
+++ etc/spring-cloud-msvcs-example/customer/src/main/java/net/henrypost/customer/repository/CustomerRepository.java
@@ -3,5 +3,5 @@ package net.henrypost.customer.repository;
 import net.henrypost.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
