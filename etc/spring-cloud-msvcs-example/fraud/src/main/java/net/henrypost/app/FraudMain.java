0b65cf67f5c9c86911dd54c7a3938cb138adae4c
Henry Post <HenryFBP@gmail.com>
2022-06-15 01:59:37 +0000

    add fraud module

---
+++ etc/spring-cloud-msvcs-example/fraud/src/main/java/net/henrypost/app/FraudMain.java
@@ -0,0 +1,7 @@
+package net.henrypost.app;
+
+public class FraudMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!" + FraudMain.class.getSimpleName());
+    }
+}
