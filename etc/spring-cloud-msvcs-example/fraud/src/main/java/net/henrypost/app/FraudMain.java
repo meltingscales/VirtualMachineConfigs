fabdd93718ce4db3a4547a41f348a26a6abba59a
Henry Post <HenryFBP@gmail.com>
2022-06-15 02:13:23 +0000

    start fraud

--- etc/spring-cloud-msvcs-example/fraud/src/main/java/net/henrypost/app/FraudMain.java
+++
@@ -1,7 +0,0 @@
-package net.henrypost.app;
-
-public class FraudMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world!" + FraudMain.class.getSimpleName());
-    }
-}
---
+++ etc/spring-cloud-msvcs-example/fraud/src/main/java/net/henrypost/fraud/FraudApp.java
@@ -0,0 +1,11 @@
+package net.henrypost.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApp.class, args);
+    }
+}
