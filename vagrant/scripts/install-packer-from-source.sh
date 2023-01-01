d102e96ae026754eeae68da815f462540d2deb78
henryfbp <HenryFBP@gmail.com>
2023-01-01 09:24:46 -0500

    improve message for packer compile

--- vagrant/scripts/install-packer-from-source.sh
+++ vagrant/scripts/install-packer-from-source.sh
@@ -12,7 +12,7 @@ if [ ! "$?" -eq "0" ]; then # if exit code is nonzero, it is not a command
     wget https://github.com/hashicorp/packer/archive/refs/tags/$VERSION.zip
     unzip $VERSION.zip
     cd ./packer-*/
-    echo "yay, time to wait :) gompilation dime :DDDD"
+    echo "Time to wait for go to compile packer binary!"
     go mod download
     mkdir -p ./bin/
     go build -o bin/packer ./
